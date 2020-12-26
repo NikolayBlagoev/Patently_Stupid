3725f1c55a54be49427d941a8c39b6ec3d976164
von slagzee <AliHossamedinAliAbdelazizElsayed@student.tudelft.nl>
2020-12-26 20:07:21 +0100

    splashscreen, 99% done

--- public/script.js
+++ public/script.js
@@ -1,47 +1,31 @@
 function press(event) {
-    if (event.keyCode == 13) {
-		play();
-    }
+    if (event.keyCode == 13) play(); 
 }
 
 function play(){
-	console.log("hekk")
 	var name = document.getElementById("uname").value;
-	var roomcode = document.getElementById("roomcode").value.toUpperCase();
-	if(roomcode==""){
+	var roomcode = document.getElementById("roomcode").value;
+
+	if(roomcode == ""){
 		alert("Please, enter a room code");
-		return;
-	} 
-	if(roomcode.includes("GAME")){
-		roomcode = roomcode.split("?GAME=")[1];
-	
-	}
-	//this method doesnt work
-	if(roomcode.length>3) {
-		alert("Invalid room code");
 		return;
 	} 
 	
 	if(name != ""){
-		document.cookie;
 		document.cookie = "user=" + name + ";";
-		document.cookie;
-
-		window.location.href='../play/?game='+roomcode;
-	} else alert("enter a username");
+		window.location.href = '../play/?game=' + roomcode;
+	} 
+	else alert("enter a username");
 }
 
 function create(){
 	var name = document.getElementById("uname").value;
 	if(name != ""){
-		document.cookie;
 		document.cookie = "user=" + name + ";";
-		document.cookie;
-		window.location.href='../create';
-	} else alert("enter a username");
+		window.location.href = '../create';
+	} 
+	else alert("enter a username");
 }
-
-
 
 window.onload = function()
 {
@@ -55,17 +39,38 @@ window.onload = function()
 	var span = document.getElementsByClassName("close")[0];
 	
 	btn.onclick = function() {
-	  modal.style.display = "block";
+		modal.style.display = "block";
 	}	
-	span.onclick = function() {
-	  modal.style.display = "none";
+
+	span.onclick = function() { 
+		modal.style.display = "none";
 	}
+
 	window.onclick = function(event) {
-	  if (event.target == modal) {
-	    modal.style.display = "none";
-	  }
+		if (event.target == modal) { 
+			modal.style.display = "none"; 
+		}
 	}
 }
 
 
+function roomcodeChanged(){
+	var roomcode = document.getElementById("roomcode");
+	roomcode.value = roomcode.value.toUpperCase();
 
+	if(roomcode.value.includes("GAME")){
+		roomcode.value = roomcode.value.split("?GAME=")[1];
+	}
+
+	if(roomcode.value.length >= 3) {
+		roomcode.value = roomcode.value.substr(0,3);
+	} 	
+}
+
+function selectUname(){
+	document.getElementById("uname").focus();
+}
+
+function selectRoomCode(){
+	document.getElementById("roomcode").focus();
+}
